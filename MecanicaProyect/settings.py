e3671be62822a9d2e913887908a8666fbdeb2960
samivt <melani.vilana@epn.edu.ec>
2024-06-18 00:42:33 -0500

    Cambio logos

--- MecanicaProyect/settings.py
+++ MecanicaProyect/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 
@@ -144,7 +144,10 @@ USE_TZ = True
 STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
-
+# Ubicación de los archivos estáticos en el sistema de archivos
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MIGRATION_MODULES = {
